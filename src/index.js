ec6b98a7558309318c628fa7bdad22bfdb00af47
KhangNguyen <khangng872@gmail.com>
2025-04-09 12:58:18 -0400

    update

--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
 import { PrimeReactProvider } from 'primereact/api';
-import { AuthProvider } from '../src/context/AuthContext.js';
 
 createRoot(
   document.getElementById("root")
 ).render(
-    <AuthProvider>
-      <PrimeReactProvider>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-      </PrimeReactProvider>
-    </AuthProvider>
+    <PrimeReactProvider>
+      <React.StrictMode>
+          <App />
+      </React.StrictMode>
+    </PrimeReactProvider>
 );
 
 // Measure performance and log results (e.g., console.log or send to an analytics endpoint)
